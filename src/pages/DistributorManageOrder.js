841ce20c5c84abda973e649f4c4ae6b7c2774065
Felipe <felipe.delatorre@effectual.com>
2022-04-14 21:45:04 -0700

    stuff

---
+++ src/pages/DistributorManageOrder.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+// import { Flex } from '@aws-amplify/ui-react';
+// import Modal from 'react-modal';
+
+export default function DistributorManageOrder() {
+  // Modal.setAppElement('body');
+
+  return <p>hello</p>;
+}
