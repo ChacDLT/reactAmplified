836d1b9dc977ca8f65aa7a7ea32fcd32a98e41ee
Felipe <felipe.delatorre@effectual.com>
2022-04-17 09:17:09 -0700

    More stuff

--- src/layouts/BBLayout.js
+++ src/layouts/BBLayout.js
@@ -4,7 +4,7 @@ import Button from '../components/Button';
 import Drawer from '../components/Drawer';
 
 const Navbar = () => (
-  <div className="relative h-20 flex flex-col-reverse md:bg-white md:w-11/12 xl:w-full mx-auto ">
+  <div className="relative h-20 flex flex-col-reverse md:bg-white xl:w-full mx-auto ">
     <div className="absolute bottom-0 flex flex-row pl-2">
       <img src={kiwi} alt="" className="w-20" />
       <div className="my-auto">KIWI Corp</div>
@@ -22,15 +22,23 @@ export default function BBLayout({ children }) {
   const [isOpenMessageDrawer, setIsOpenMessageDrawer] = useState(false);
 
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-lg md:w-11/12 mx-auto">
       <Navbar />
-      <div className="content-center">
-        <Button className="md:invisible" onClick={() => setIsOpenMessageDrawer(true)}>
+      <div className="md:hidden flex justify-center mt-2">
+        <Button className="" onClick={() => setIsOpenMessageDrawer(true)}>
           Open Messages
         </Button>
       </div>
+
+      <div className="flex flex-row">
+        <div className="hidden md:block">messages here</div>
+        <div>
+          <div>orderInfo here</div>
+          <div>{children}</div>
+        </div>
+      </div>
+
       <Drawer isOpen={isOpenMessageDrawer} setIsOpen={setIsOpenMessageDrawer} />
-      {children}
     </div>
   );
 }
